e35e2a0ec8d7cc7d4eb6120484a55876c1a46185
YashHarale <yashharale2003@gmail.com>
2024-07-16 18:49:35 +0530

    Error Handling - related to SSL cert

--- index.py
+++ index.py
@@ -1,2 +1,19 @@
 import requests
-from bs4 import BeautifulSoup
+import certifi
+
+url = 'https://hprera.nic.in/PublicDashboard'
+try:
+    response = requests.get(url, verify=certifi.where())
+    print(response.content)
+except requests.exceptions.SSLError as e:
+    print("SSL Error:", e)
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+# print(response.text)
+
+# registered_projects = soup.find_all('div', class_='your-registered-projects-class')
+
+# # Assuming that the RERA numbers are links that take you to the detail page
+# projects = registered_projects.find_all('a', href=True)[:6]
+
